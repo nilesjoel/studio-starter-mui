3796e0484d7e38f6c1a7c0a639d42ae76f6348b3
Niles Joel White <nilesjoel@gmail.com>
2024-01-29 17:26:17 +0000

    feat: Format code ' and 'index.tsx' and add 'feature.tsx' page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '../src/Link';
-import ProTip from '../src/ProTip';
-import Copyright from '../src/Copyright';
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Link from "../src/Link";
+import ProTip from "../src/ProTip";
+import Copyright from "../src/Copyright";
 
 export default function Home() {
   return (
@@ -12,17 +12,17 @@ export default function Home() {
       <Box
         sx={{
           my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
         <Typography variant="h4" component="h1" gutterBottom>
-         Studio UI : TypeScript, NextJS, MUI,StyledComponents, 
+          Studio UI : TypeScript, NextJS, MUI,StyledComponents,
         </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
+        <Link href="/feature" color="secondary">
+          Go to the Feature Page
         </Link>
         <ProTip />
         <Copyright />
